0d0a86714be71ffd29a0f26465462ed86e40a292
Ryan Gaus <rsg1egoman@gmail.com>
2025-04-28 16:42:59 -0400

    feat: clean up frontend interface to make it look a little nicer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,18 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { LoaderIcon } from "lucide-react";
 import debounce from 'lodash.debounce';
-import { Advocate } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
+
+import { Advocate } from "@/lib/types";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 const SEARCH_TEXT_DEBOUNCE_RATE_MS = 500;
 
@@ -38,64 +48,89 @@ export default function Home() {
     setSearchText(e.currentTarget.value);
   }, []);
 
-  const onResetSearch = useCallback(() => {
-    setSearchText('');
-  }, []);
-
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <input style={{ border: "1px solid black" }} value={searchText} onChange={onSearchChange} />
-        <button onClick={onResetSearch}>Reset Search</button>
+    <main className="fixed inset-0 flex flex-col gap-4">
+      <div className="flex items-center justify-center border-b">
+        <div className="flex items-center justify-between h-12 w-full max-w-[1200px] px-2">
+          <h1 className="font-medium">Solace Advocates</h1>
+        </div>
       </div>
-      <br />
-      <br />
-      {status === 'pending' ? (
-        <div>
-          <LoaderIcon size={32} className="animate-spin" />
+
+      <div className="flex justify-center overflow-auto">
+        <div className="flex flex-col items-center w-full max-w-[1200px] px-2">
+          <div className="flex justify-end items-center w-full h-10">
+            {/* FIXME: replace the below input with a shadcn component or something similar */}
+            <input
+              style={{ border: "1px solid black" }}
+              value={searchText}
+              onChange={onSearchChange}
+              placeholder="Search"
+            />
+          </div>
+
+          {status === 'pending' ? (
+            <div>
+              <LoaderIcon size={32} className="animate-spin" />
+            </div>
+          ) : null}
+          {status === 'error' ? (
+            <div>
+              <span className="text-red-500">Error loading advocates!</span>
+            </div>
+          ) : null}
+          {status === 'success' ? (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>First Name</TableHead>
+                  <TableHead>Last Name</TableHead>
+                  <TableHead>City</TableHead>
+                  <TableHead>Degree</TableHead>
+                  <TableHead>Specialties</TableHead>
+                  <TableHead>Years of Experience</TableHead>
+                  <TableHead>Phone Number</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {data.map((advocate) => {
+                  return (
+                    <TableRow key={advocate.id}>
+                      <TableCell>{advocate.firstName}</TableCell>
+                      <TableCell>{advocate.lastName}</TableCell>
+                      <TableCell>{advocate.city}</TableCell>
+                      <TableCell>{advocate.degree}</TableCell>
+                      <TableCell>
+                        <div className="flex flex-col gap-2">
+                          {advocate.specialties.map((s) => (
+                            <div key={s}>
+                              <div className="inline-flex items-center bg-gray-800 px-2 h-6 rounded-full">
+                                <span className="text-xs text-gray-100 truncate">{s}</span>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </TableCell>
+                      <TableCell>{advocate.yearsOfExperience}</TableCell>
+                      <TableCell>{advocate.phoneNumber}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+              {data.length === 0 ? (
+                <TableBody>
+                  <TableRow>
+                    <TableCell colSpan={7}>
+                      <div className="flex items-center justify-center h-20">
+                        <span className="text-gray-500">No advocates found!</span>
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              ) : null}
+            </Table>
+          ) : null}
         </div>
-      ) : null}
-      {status === 'error' ? (
-        <div>
-          <span className="text-red-500">Error loading advocates!</span>
-        </div>
-      ) : null}
-      {status === 'success' ? (
-        <table>
-          <thead>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </thead>
-          <tbody>
-            {data.map((advocate) => {
-              return (
-                <tr>
-                  <td>{advocate.firstName}</td>
-                  <td>{advocate.lastName}</td>
-                  <td>{advocate.city}</td>
-                  <td>{advocate.degree}</td>
-                  <td>
-                    {advocate.specialties.map((s) => (
-                      <div>{s}</div>
-                    ))}
-                  </td>
-                  <td>{advocate.yearsOfExperience}</td>
-                  <td>{advocate.phoneNumber}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      ) : null}
+      </div>
     </main>
   );
 }
